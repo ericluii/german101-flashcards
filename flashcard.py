2bff3373b771b9e56c271b763c350fea9a61ddf7
Chris Ngan <chris.ngan24@gmail.com>
2013-12-10 19:08:06 -0500

    added more vocab, added option to use files starting with particular strings

--- flashcard.py
+++ flashcard.py
@@ -1,23 +1,27 @@
 import sys, csv, random, os
 
 # reads from the path, returns list of words
-def csv_reader(path):
+def csv_reader(path, file_name):
 	words = []
 	if len(path.split('.')) > 1:	
 		write_to_array(words, open(path, 'rb'))
 	else:
-		read_files_to_array(words, path)
+		read_files_to_array(words, path, file_name)
 	return words
 
 # recursive function that reads all items in directory
 # writes words to word array
-def read_files_to_array(words, path):
+def read_files_to_array(words, path, file_name):
 	items = os.listdir(path)
 	for item in items:
-		if len(item.split('.')) > 1:
-			write_to_array(words, open(path + '/'+item, 'rb'))	
+		if len(item.split('.')) > 1:			
+			if len(file_name) == 0:
+				write_to_array(words, open(path + '/'+item, 'rb'))	
+			if (len(file_name) > 0 and item.find(file_name)!= -1):
+				print path+'/'+item
+				write_to_array(words, open(path + '/'+item, 'rb'))	
 		else:
-			read_files_to_array(words, path + '/' + item)
+			read_files_to_array(words, path + '/' + item, file_name)
 
 # writes from csv file to word array
 def write_to_array(words, file):
@@ -29,12 +33,17 @@ def write_to_array(words, file):
 		})
 
 def main():
-	if len(sys.argv) < 3:
-		print 'Requires a csv file and flash mode, leaving now...'
+	if len(sys.argv) < 4:
+		print 'Requires a csv file, flash mode and file name, leaving now...'
+		print 'if you want all, put \'\' as last arg'
 		exit(1)
 	path = sys.argv[1]
-	mode = sys.argv[2]
-	words = csv_reader(path)
+	mode = sys.argv[2]		
+	file_name = sys.argv[3]		
+	if file_name == "''":
+		file_name = ''
+	words = csv_reader(path, file_name)
+
 		
 
 	answer = 'first'
